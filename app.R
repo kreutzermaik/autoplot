8b2ea138d209d6caaa705d30177a64e174076ac0
Maik Kreutzer <kreutzermaik123@web.de>
2021-11-02 18:58:12 +0100

    Initial commit

---
+++ app.R
@@ -0,0 +1,90 @@
+# Libraries
+library(ggplot2)
+library(dplyr)
+library(hrbrthemes)
+library(viridis)
+library(ggpubr)
+library(hms)
+library(gridExtra)
+
+# Path
+base_path <- "C:/Users/kreut/OneDrive/Uni/Bachelor-Thesis/Quellcode/CSV-Analyser/"
+export_path <- "C:/Users/kreut/OneDrive/Uni/Bachelor-Thesis/Quellcode/CSV-Analyser/export/"
+date_path <- toString(Sys.Date())
+
+
+# Colors
+axisLabelColor <- "#69b3a2"
+yAxisColor <- "black"
+
+
+# Import CSV from Collectl
+data_csv <- read.csv("collectl_text.txt", na="NA")
+
+
+# Put measurements into dataframe
+measurements <- data.frame(
+    time=data_csv[[2]], 
+    cpu_user=data_csv[[3]],
+    cpu_nice=data_csv[[4]],
+    cpu_system=data_csv[[5]],
+    cpu_wait=data_csv[[6]],
+    cpu_idle=data_csv[[15]]
+)
+
+
+# function to draw line plots
+drawPlot <- function(title, yAxisLabel, measurement, time) {
+    ggplot(measurements, aes(x=time, y=measurement, group=1)) +
+        geom_line() +
+        scale_y_continuous(
+            name = yAxisLabel,
+        ) +
+        theme_ipsum() +
+        theme(
+            axis.title.y = element_text(color = axisLabelColor, size=15),
+            axis.title.x = element_text(color = axisLabelColor, size=15),
+            axis.text.x = element_blank(),
+            axis.ticks.x = element_blank(),
+            legend.position = "none",
+        ) +
+        ggtitle(title)
+}
+
+removeWhitespaceAndColon <- function(filename) {
+  filename <- chartr(":", "-", filename)
+  filename <- chartr(" ", "_", filename)
+}
+
+
+# call drawPlot function for each plot
+cpu_user_plot <- drawPlot("CPU User", "CPU-Verbrauch (%)", measurements$cpu_user, measurements$time)
+cpu_nice_plot <- drawPlot("CPU Nice", "CPU-Verbrauch (%)", measurements$cpu_nice, measurements$time)
+cpu_system_plot <- drawPlot("CPU System", "CPU-Verbrauch (%)", measurements$cpu_system, measurements$time)
+cpu_wait_plot <- drawPlot("CPU Wait", "CPU-Verbrauch (%)", measurements$cpu_wait, measurements$time)
+cpu_idle_plot <- drawPlot("CPU Idle", "CPU-Verbrauch (%)", measurements$cpu_idle, measurements$time)
+
+
+# Build figures with multiple plots
+figure <- ggarrange(
+  cpu_user_plot, cpu_nice_plot, cpu_system_plot, cpu_wait_plot, cpu_idle_plot, nrow = 5
+)
+
+
+# Create directories for exports with todays date and save figures into it
+dir.create(file.path(export_path), showWarnings = FALSE)
+
+dir.create(file.path(export_path, date_path), showWarnings = FALSE)
+setwd(file.path(export_path, date_path))
+
+# get filename
+output_name <- removeWhitespaceAndColon(paste(Sys.time(), "CPU_figure.png"))
+
+ggexport(figure, filename = paste(output_name), width = 1920, height = 1080)
+
+
+
+
+
+
+
